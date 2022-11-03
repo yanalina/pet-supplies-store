a27d4e489d4c9d83ece1534e90fa78d56956718c
luis-pad <115506242+luis-pad@users.noreply.github.com>
2022-11-03 16:44:53 -0700

    Create IncrementDecrementServlet.java

---
+++ src/main/java/pets/servlet/IncrementDecrementServlet.java
@@ -0,0 +1,66 @@
+package pets.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+import pets.model.Cart;
+
+
+/**
+* Servlet implementation class IncrementDecrementServlet
+*/
+public class IncrementDecrementServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter();){
+			String action = request.getParameter("action");
+			int id = Integer.parseInt(request.getParameter("product_id"));
+			
+			@SuppressWarnings("unchecked")
+			ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+			
+			if(action != null && id >= 1) {
+				if(action.equals("inc")) {
+					for(Cart c:cart_list) {
+						if(c.getId() == id) {
+							int quantity = c.getQuantity();
+							quantity++;
+							c.setQuantity(quantity);
+							response.sendRedirect("cart.jsp");
+						}
+					}
+				}
+				
+				
+					if(action.equals("dec")) {
+						for(Cart c:cart_list) {
+							if(c.getId() == id && c.getQuantity() > 1) {
+								int quantity = c.getQuantity();
+								quantity--;
+								c.setQuantity(quantity);
+								break;
+							}
+						}
+						response.sendRedirect("cart.jsp");
+					}
+			}
+			else {
+				response.sendRedirect("cart.jsp");
+			
+			
+			}
+			
+		}
+	}
+
+}
