7caa033d3c129a4a6b8515bd6f8118da9860498f
yanalina <83517654+yanalina@users.noreply.github.com>
2022-11-09 22:08:45 -0800

    Updated ProductDao
    Added method to filter by category and subcategory.

--- src/main/java/pets/dao/ProductDao.java
+++ src/main/java/pets/dao/ProductDao.java
@@ -40,6 +40,60 @@ public class ProductDao {
 		return products;
 	}
 	
+	public List<Product> getAllProductsById (int categoryId) {
+		List<Product> products = new ArrayList<Product>();
+		
+		try {
+			query = "Select * from products where category_id=?";
+			pst = this.con.prepareStatement(query);
+			pst.setInt(1, categoryId);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				Product row = new Product();
+				row.setId(rs.getInt("product_id"));
+				row.setTitle(rs.getString("product_title"));
+				row.setDescription(rs.getString("product_description"));
+				row.setImage(rs.getString("product_image"));
+				row.setPrice(rs.getDouble("product_price"));
+				row.setCategory(rs.getString("product_category"));
+				row.setSubcategory(rs.getString("product_subcategory"));
+				
+				products.add(row);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return products;
+	}
+	
+	public List<Product> getAllProductsByIdSub (int subcategoryId) {
+		List<Product> products = new ArrayList<Product>();
+		
+		try {
+			query = "Select * from products where subcategory_id=?";
+			pst = this.con.prepareStatement(query);
+			pst.setInt(1, subcategoryId);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				Product row = new Product();
+				row.setId(rs.getInt("product_id"));
+				row.setTitle(rs.getString("product_title"));
+				row.setDescription(rs.getString("product_description"));
+				row.setImage(rs.getString("product_image"));
+				row.setPrice(rs.getDouble("product_price"));
+				row.setCategory(rs.getString("product_category"));
+				row.setSubcategory(rs.getString("product_subcategory"));
+				
+				products.add(row);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return products;
+	}
+	
 	public List<Cart> getCartProducts(ArrayList<Cart> cartList){
 		List<Cart> products = new ArrayList<Cart>();
 	
